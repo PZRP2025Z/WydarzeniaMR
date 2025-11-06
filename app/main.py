fc7818e7365e14bbe1d8f7150cb15947ad07af2e
bmichal1 <01178564@pw.edu.pl>
2025-11-06 22:48:23 +0100

    Small temporary fix

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.routes import event_routes
 from .logger import setup_logging
 from .database.session import engine
 
+app = FastAPI()
 
 logger = logging.getLogger(__name__)
 setup_logging()
@@ -16,7 +17,12 @@ app = FastAPI(title="WydarzeniaMR_API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # adres frontendu Svelte
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+    ],  # adres frontendu Svelte
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, PUT, DELETE, OPTIONS
     allow_headers=["*"],
