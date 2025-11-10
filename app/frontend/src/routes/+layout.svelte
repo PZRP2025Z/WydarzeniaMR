c7ec37a0b3146108eaf080c7b44943f37e9956e9
bmichal1 <01178564@pw.edu.pl>
2025-11-10 23:52:34 +0100

    Language button is used on all pages, added navigation, moved adding event to separate page

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { lang } from '$lib/stores/stores';
+	import { t } from '$lib/i18n';
 
 	let { children } = $props();
+
+	function toggleLang() {
+		lang.set($lang === 'pl' ? 'en' : 'pl');
+	}
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<nav>
+    <a href="/">{t('home_title', $lang)}</a>
+    <a href="/events">{t('add_event', $lang)}</a>
+</nav>
+
+<button onclick={toggleLang}>
+	{ $lang === 'pl' ? 'EN' : 'PL' }
+</button>
 
 {@render children()}
