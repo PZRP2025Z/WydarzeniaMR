7e54003c5e8eb2692d1df97cf4b59d264d30a7e5
Maciej PC <maciej_pc@gmail.com>
2025-12-11 20:36:35 +0100

    comments & markdown

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import logging
-import time
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,30 +17,31 @@ logger = logging.getLogger(__name__)
 setup_logging()
 logger.info("Starting Application")
 
-app = FastAPI(title="WydarzeniaMR_API")
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    logger.info("Database initialized")
+    yield
+    logger.info("Shutting down application")
+
+
+app = FastAPI(title="WydarzeniaMR_API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-def init_db():
-    SQLModel.metadata.create_all(engine)
-
-
-@app.on_event("startup")
-def on_startup():
-    init_db()
-
 
 app.include_router(event_routes.router)
 app.include_router(auth_routes.router)
